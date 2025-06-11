a4864d815b90fb624dade8066c7aa4b00a193659
Offsprey14 <offsprey14@gmail.com>
2025-06-11 07:59:50 -0700

    Implemented redirection to new edit form

--- modules/custom/contentdup/src/Controller/DupController.php
+++ modules/custom/contentdup/src/Controller/DupController.php
@@ -17,38 +17,44 @@ use Drupal\Core\Url;
 class DupController extends ControllerBase {
   public function duplicate($title, $nid) {
     //exclude internal / metadata fields
-    $exemptFields = ['nid', 'vid', 'type', 'uuid', 'langcode', 'revision_uid', 'revision_log',
+    $exemptFields = ['nid', 'vid', 'type', 'uuid', 'title', 'langcode', 'revision_uid', 'revision_log',
     'revision_default', 'revision_timestamp', 'isDefaultRevision', 'status', 'uid', 'created',
     'changed', 'promote', 'sticky'];
-    //lookup parent ndoe from nid    
+
+    //lookup origin node from nid    
     $pnode = Node::load($nid);
     $newTitle = 'Copy of ' . $title;
     $node = Node::create([
       'type' => 'article', // Specify the content type
       'title' => $newTitle, // Set the title
 ]);
-//iterate origin node fields and copy to new node, ignoring exempt fields
-foreach($pnode->getFields() as $field){
-    $fieldName = $field->getName();
-    if(!in_array($fieldName, $exemptFields)){
-    //dpm($field->getValue());
-    $node->set($fieldName,$field->getValue());
-  }  
-}
-$node->set('title', $newTitle);
+    //iterate origin node fields and copy to new node, ignoring exempt fields
+    foreach($pnode->getFields() as $field){
+        $fieldName = $field->getName();
+        if(!in_array($fieldName, $exemptFields)){
+        //dpm($field->getValue());
+          $node->set($fieldName,$field->getValue());
+      }  
+    }
 
-// Save the node.
-$node->save();
-$nid = $node->id();
-$edit_url = Url::fromRoute('entity.node.edit_form', ['node' => $nid]);
-$link = Link::fromTextAndUrl('Edit this node', $edit_url)->toString();
-    /* return [
-  '#markup' => t('Node created "%title" - %nid. @edit_link', [
-    '%title' => $newTitle,
-    '%nid' => $nid,
-    '@edit_link' => $link,
-  ]),
-]; */
-  return new RedirectResponse($edit_url->toString());
-  }
+    // Save the node.
+    $node->save();
+    $newNid = $node->id();
+
+    //build response
+    $edit_url = Url::fromRoute('entity.node.edit_form', ['node' => $newNid]);
+    $link = Link::fromTextAndUrl('Edit this node', $edit_url)->toString();
+        /* return [
+      '#markup' => t('Node created "%title" - %nid. @edit_link', [
+        '%title' => $newTitle,
+        '%nid' => $newNid,
+        '@edit_link' => $link,
+      ]),
+    ]; */
+      //return new RedirectResponse($edit_url->toString());
+      
+      $response = new RedirectResponse($edit_url->toString());
+      $response->send();
+      return $response;
+    }
 }
